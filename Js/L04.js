7b61b17853f8fd636a38276bf5e4ded2db16c2fe
ll222nm <Lojzzan@wl1282.lnu.se>
2013-12-15 16:24:45 +0100

    Laboration 4

--- Js/L04.js
+++ Js/L04.js
@@ -9,7 +9,7 @@ function test1() {
 	var tal =10;
 	
 	return tal;
-	// jag har skapat en variabeln och tilldelat variabeln tal
+	// jag har skapat en variabeln och tilldelat värdet 10. Sedan returnerar jag talet.
 	
 }
 
@@ -21,7 +21,7 @@ function test1() {
 // Multiplicera sedan de två variablerna, returnera produkten
 function test2() {
 	var tal1 = 2.5;
-	var tal2 =8
+	var tal2 =8;
 	var resultat=2.5*8;
 	return resultat;
 	// jag har skapat två variabler och sedan multipliserat de numerska värderna med varandra och retunerat resultat.
@@ -39,7 +39,7 @@ function test3(tal1, tal2) {
 	
 	var resultat =tal1-tal2;
 	return resultat;
-	// jag har minskat tal1 med tal 2 och sedan returnerat resultatet.
+	//  Jag har skapat en variabel som heter resultat och sedan har jag minskat tal1 med tal 2 och sedan returnerat resultatet.
 	
 }
 
@@ -64,7 +64,7 @@ function test4(tal) {
 function test5(text) {
 	
  return text.length;
- // jag retunerade längden på variabeln.
+ // jag retunerade längden på strängen i variabeln.
 	
 }
 
@@ -75,9 +75,11 @@ function test5(text) {
 */
 // Returnera den näst sista bokstaven i textsträngen som kommer in i parametern text
 function test6(text) {
+
+	var nästsista = text.length - 1;
+	return text.charAt(nästsista - 1);
 	
-	return text.charAt(text.length -2);
-	// Jag retunerade näst sista bokstaven genom att skriva -2. Därför att då går jag tillbaka tvåsteg och hamnar då på den näst sista bokstaven "x".
+	// Jag skapat en varabel som heter " nästsista" och det är lika med textlängden -1. För att få fram vilket nummer näst sista bokstaven har. Sedan retunerade jag nästsista bokstaven genom att skriva (text.charAt(nästsista - 1). Därför att då går jag tillbaka tvåsteg och hamnar då på den näst sista bokstaven.
 }
 
 
@@ -91,11 +93,11 @@ function test6(text) {
 function test7(firstname, surname) {
 	
 
-var namn = surname + ", "+firstname ;
+var namn = surname + ", " + firstname ;
 return namn; 
 }
+//  Jag skapade en variable somm heter namn och skrev det är lika med "surname+ ", "+ firstnamne" och sedan gjorde jag en return på namn.
 
-//  
 
 /*
 * TEST 8
@@ -113,9 +115,12 @@ function test8(words) {
 	return word;
 
 }
+// jag skapade två variablar  och tog hela parametern words och letade upp vilken plats det första mellanslaget finns, den har jag skapat i number.
+// Sedan skapade jag en variabel till som heter word och där lägger jag till plus ett, för att komma till början av ordet. Sedan skriver jag return word.
+
 
 /*
-* TEST 9http://orion.lnu.se/pub/education/course/1ME402/ht13/rat%20base.jpeg
+* TEST 9
 */
 // Om numret i parametern number är större eller lika med 100 ska en boolean true returneras
 // annars ska en boolean false resturneras
@@ -125,12 +130,13 @@ function test9(number) {
 		return true;
 	}
 	else
-	
-{return false;}
+	{
+		return false;
+	}
 
+// Först skriver jag if för att ta reda på om det som parantesen är sann, då skriver jag return true.Om det inte är sant så skriver jag else och sedan return false.
 }
 
-// Först skriver jag if för att ta reda på om det som parantesen är san, då skrier jag return true.Om det inte är sant så skriver jag else och sedan return false.
 
 /*
 * TEST 10
@@ -157,6 +163,9 @@ else{
 }
 
 }
+//Först kollar jag ifall epost längden är mindre än 1 eller om namn längden är mindre än 1. Om det är det, blir det return " Du har glömt att ange namn eller epost."
+// Jag skriver else if därför att kolla om det finns ett @ i epost.Och finns det ett @ så går den vidare till else. Finns det inte det så hoppar det över else-
+//Jag skriver else därför att om det är större än 1 så kommer inte det som var i if-satsen att gå igenom. Sedan skriver jag return "Ditt medelande har skickats" 
 
 /*
 * TEST 11
@@ -173,7 +182,11 @@ function test11(arr) {
 	middle=Math.round(middle);
 	return arr[middle];
 }
-
+// Först skapar jag en variabel som heter "middle" och den är lika med arrayens längd. 
+// Sedan skriver jag middle/2 för att leta upp det mittersta värdet.
+// Sedan tar jag det minus 1.
+// Sedan avrundar jag "middle" t.ex. om middle är 2.5 så blir det efter math.round 3.
+// Sedan retunerar jag arrayens mittersta värde.
 
 /*
 * TEST 12
@@ -191,6 +204,10 @@ function test12(arr) {
 	return summa/length;
 }
 
+// Först s¨skapar jag en variabel som heter "summa".  som är lika med 0.
+// Sedan skapar jag en variabel som heter "length" och den är i lika med arrayens lägnd.
+// Sedan skapar jag en for-loop som lägger till varje tal i arrayen på summan, då man vill få ut värderna i arrayen.
+// Sedan skriver jag return summa delat på längden , därför att jag ska räkna ut medelvärdet, då tar jag summan av alla tal och dela på antalet.
 
 
 
