bed003fec3b7988ec1b51119c8f1a1b50e438c84
ll222nm <Lojzzan@wl1198.lnu.se>
2013-12-20 10:01:05 +0100

    Uppgift 2 - Valutakonverteraren

--- Js/valuta.js
+++ Js/valuta.js
@@ -1,15 +1,45 @@
-
-document.getElementById("calculate_button").onclick = function(){
-
-var input=document.getElementById("input_number").value;
 var dollarkurs = 0.15;
 var eurokurs = 0.11;
-
-var message = input + " kr är " + input*eurokurs+" euro och " + input*dollarkurs + " dollar";
-
-document.getElementById("number").innerHTML = message;
+document.getElementById("calculate_button").onclick = function(){
+calculate_exchangerate();
 
 };
+
+
+function calculate_dollar(sek){
+return sek*dollarkurs;	
+}
+function calculate_euro(sek){
+return sek*eurokurs;	
+}
+
+function calculate_exchangerate(){
+	var input=document.querySelector("#input_number");
+	var sek=input.value;
+	if(isNaN(sek)){
+		alert("Du får bara skriva i siffror i rutan");
+		
+	}
+	else{
+		addnewlistvaluement(sek);
+	}
+}
+
+function addnewlistvaluement(sek){
+	var dollar=calculate_dollar(sek).toFixed(2);
+	var euro=calculate_euro(sek).toFixed(2);
+	var list=document.querySelector("#tidigare");
+	
+	var li=document.createElement("li");
+	
+	var line=sek+" sek = "+euro+" euro, "+dollar +" dollar";
+	
+	var text=document.createTextNode(line);
+	
+	li.appendChild(text);
+	
+	list.insertBefore(li,list.firstChild);
+}
 
 
 
